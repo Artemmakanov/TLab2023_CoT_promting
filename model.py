b71dcd612e3cd3d109d7b9e2482a96bafc5521e6
Artemmakanov <artem.makanov@mail.ru>
2023-02-27 20:04:38 +0300

    6 commit

--- model.py
+++ model.py
@@ -46,8 +46,6 @@ class Model:
 
         set_seed(seed)
 
-
-
         with open('promts.jsonl', 'r') as f:
             self.prompts = json.loads(f.read())
 
@@ -79,9 +77,6 @@ class Model:
                 outputs = self.model_bert(inputs, output_hidden_states=True)
                 prompts_question_emb = outputs.hidden_states[6][0][0]
                 self.prompts_question_embs.append(prompts_question_emb)
-            
-
-        
         
         self.suffix = "The answer is"
         # Символы, при появлении которых генерация модели прекращается.
@@ -101,6 +96,7 @@ class Model:
         @question - (str) - Вопрос, на который следует найти ответ
         @return - (str) - Строка, которую уже можно подавать в модель.
         """
+        
         return "Question: {0}\nAnswer: {1}\nQuestion: {2}\nAnswer: ".format(
             prompt['question'], prompt['answer'], question)
           
@@ -112,6 +108,7 @@ class Model:
         @answer - (str) - Ответ модели, ее CoT,
         @return - (str) - Строка, которую следует подать в модель
         """
+
         return "Question: {0}\nAnswer: {1}\nQuestion: {2}\nAnswer: {3} {4} ".format(
             prompt['question'], prompt['answer'], question, answer, self.suffix)
 
@@ -124,36 +121,30 @@ class Model:
         @return - (str) - ответ модели.
         """
 
-
         inputs = self.tokenizer(input_str, return_tensors="pt")["input_ids"]
 
         if self.gpu:
             inputs = inputs.cuda()
 
-
         outputs_lst = []
         
         # Поскольку в процессе генерации возможно превышение числа 512 токенов, применим конструкцию try
-        try:
-            with self.model.inference_session(max_length=512) as sess:
+        with self.model.inference_session(max_length=512) as sess:
+            self.kwargs["session"] = sess
 
-                self.kwargs["session"] = sess
+            while True:
 
-                while True:
+                outputs = self.model.generate(
+                        inputs, **self.kwargs)
 
-                    outputs = self.model.generate(
-                            inputs, **self.kwargs
-                        )
-        
-                    outputs = self.tokenizer.decode(outputs[0, -1:])
-                    outputs_lst.append(outputs)
+                outputs = self.tokenizer.decode(outputs[0, -1:])
+                outputs_lst.append(outputs)
 
-                    if any([stop_gen_tok in outputs for stop_gen_tok in self.stop_gen]):
-                        break
-                    inputs = None  
-        except:
-            None
-                
+                if any([stop_gen_tok in outputs for stop_gen_tok in self.stop_gen]):
+                    break
+                inputs = None  
+
+            
         return "".join(outputs_lst) 
   
     def select_idx_prompts(self, question_src):
@@ -163,9 +154,12 @@ class Model:
         @question_src - (str)
         @return - (int)
         """
+
         inputs_src = self.tokenizer_bert(question_src, return_tensors="pt")["input_ids"]
+
         if self.gpu:
             inputs_src = inputs_src.cuda()
+
         outputs_src = self.model_bert(inputs_src, output_hidden_states=True)
 
         question_src_emb = outputs_src.hidden_states[6][0][0]
@@ -175,9 +169,8 @@ class Model:
         for i in range(8):
             cos_sims.append(self.cos(question_src_emb, self.prompts_question_embs[i]).item())
         
-        
-        
         prompt_idx = list(np.argpartition(cos_sims, -5)[-5:])
+
         return prompt_idx
 
     def sec_proc(self, dataset_processer, model_size, realization, gen_regime, seed, i, example):
@@ -218,6 +211,7 @@ class Model:
                 input = self.answer_plus_suffix(prompt, example_sourse["question"], answer)
                 output = self.produce_answer(input)
                 outputs.append(output)
+                
         return outputs
 
 
